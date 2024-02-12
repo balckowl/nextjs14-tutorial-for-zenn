3fe3a8739f740a6e1c9be8d4806740c626c5ec81
balckowl <shiggyjr1492@gmail.com>
2024-02-13 03:11:10 +0900

    修正しました。

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -9,7 +9,7 @@ interface TBlog {
 const BlogPage = async () => {
 
     const getBlogData = async () => {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/blog`)
+        const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/blog`,{ cache: 'force-cache' })
     
         const data = await res.json()
     
