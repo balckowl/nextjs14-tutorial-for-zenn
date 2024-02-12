3815f17fefdc4124a5557330b53faf893e4b681c
balckowl <shiggyjr1492@gmail.com>
2024-02-13 02:35:07 +0900

    修正しました。

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -4,7 +4,7 @@ import  blogData  from "../../../blog-data.json"
 const GET = (req: NextRequest) => {
     const id = req.nextUrl.pathname.split('/').pop()
 
-    const blogArticle = blogData.find(blog => blog.id === Number(id))
+    const blogArticle = blogData.find(blog => blog.id === id)
 
     return NextResponse.json({ blogArticle })
 }
