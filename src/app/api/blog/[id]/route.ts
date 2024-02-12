aa9224d20adffc2d359c6d255d3d22e5d7d5a2f7
balckowl <shiggyjr1492@gmail.com>
2024-02-13 01:30:14 +0900

    idの取得方法を変更しました。

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 import blogData from "../../../../../blog-data.json"
 
 const GET = (req: NextRequest, { params }: { params: { id: number | string } }) => {
-    const { id } = params
+    const id = req.nextUrl.pathname.split('/').pop()
 
     const blogArticle = blogData.find(blog => blog.id === Number(id))
 
