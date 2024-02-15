de63f4f34b1bfb257a492aedcaad885b756fc95d
balckowl <shiggyjr1492@gmail.com>
2024-02-15 22:57:34 +0900

    ssgに修正しました。

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -1,18 +1,18 @@
 import BlogList from "@/app/components/blogList";
 import Loading from "@/app/loading";
-import Link from "next/link"
 import { Suspense } from "react";
 
 const BlogPage = async () => {
 
     return (
         <div className="container mx-auto py-[50px]">
-            {/* <h2>Blog</h2> */}
-            <div className="grid grid-cols-12 gap-3">
-                {/* <Suspense fallback={<Loading />}> */}
-                    <BlogList />
-                {/* </Suspense> */}
-            </div>
+            <h2 className="text-[50px] font-bold mb-5">Blog</h2>
+            <Suspense fallback={<Loading />}>
+                <BlogList waitTime={3000} />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
+                <BlogList waitTime={5000} />
+            </Suspense>
         </div>
     )
 }
--- src/app/components/blogList.tsx
+++ src/app/components/blogList.tsx
@@ -1,6 +1,4 @@
-import Loading from "@/app/loading";
 import Link from "next/link"
-import { Suspense } from "react";
 
 interface TBlog {
     id: string;
@@ -8,25 +6,23 @@ interface TBlog {
     content: string;
 }
 
-const getBlogData = async () => {
+const getBlogData = async (waitTime: number) => {
 
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    await new Promise(resolve => setTimeout(resolve, waitTime))
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/blog`,{
-        cache: 'no-store'
-    })
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/blog`)
 
     const blogData = await res.json()
 
     return blogData
 }
 
-const BlogList = async () => {
+const BlogList = async ({ waitTime }: { waitTime: number }) => {
 
-    const blogData = await getBlogData()
+    const blogData = await getBlogData(waitTime)
 
     return (
-        <>
+        <div className="grid grid-cols-12 gap-3 mb-3">
             {blogData.map((blog: TBlog) => (
                 <div className="col-span-4 border border-black rounded p-5" key={blog.id}>
                     <Link href={`/blog/${blog.id}`} className="w-full">
@@ -34,7 +30,7 @@ const BlogList = async () => {
                     </Link>
                 </div>
             ))}
-        </>
+        </div>
     )
 }
 
